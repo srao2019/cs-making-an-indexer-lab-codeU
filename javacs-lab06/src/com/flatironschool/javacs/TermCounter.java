b03e5798a70054725c2c325cb438a6db831b2c0c
Sahana Rao <sahanarao.va@gmail.com>
2016-05-19 15:41:00 -0400

    Done

--- javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
+++ javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
@@ -36,8 +36,11 @@ public class TermCounter {
 	 * @return
 	 */
 	public int size() {
-        // TODO: fill this in.
-		return -1;
+        int count = 0;
+		for(String str: map.keySet()){
+			count+=map.get(str);
+		}
+		return count;
 	}
 
 	/**
